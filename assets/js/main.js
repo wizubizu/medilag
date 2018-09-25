f37771cf297d54db80690f9697d2f0f8999e7552
wizubizu <odugbesanabimbola@gmail.com>
2018-09-25 14:43:20 +0100

    update

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+
+// parallax
+$(window).scroll(function(){
+
+    // Add parallax scrolling to all images in .paralax-image container
+    $('.parallax').each(function(){
+        // only put top value if the window scroll has gone beyond the top of the image
+        if ($(this).offset().top < $(window).scrollTop()) {
+        // Get ammount of pixels the image is above the top of the window
+        var difference = $(window).scrollTop() - $(this).offset().top;
+        // Top value of image is set to half the amount scrolled
+        // (this gives the illusion of the image scrolling slower than the rest of the page)
+        var half = (difference / 2) + 'px',
+        transform = 'translate3d( 0, ' + half + ',0)';
+
+        $(this).find('.para').css('transform', transform);
+        } else {
+          // if image is below the top of the window set top to 0
+        $(this).find('.para').css('transform', 'translate3d(0,0,0)');
+        }
+    });
+});
+
+function lightbox(idx) {
+            //show the slider's wrapper: this is required when the transitionType has been set to "slide" in the ninja-slider.js
+            var ninjaSldr = document.getElementById("ninja-slider");
+            ninjaSldr.parentNode.style.display = "block";
+
+            nslider.init(idx);
+
+            var fsBtn = document.getElementById("fsBtn");
+            fsBtn.click();
+        }
+
+        function fsIconClick(isFullscreen, ninjaSldr) { //fsIconClick is the default event handler of the fullscreen button
+            if (isFullscreen) {
+                ninjaSldr.parentNode.style.display = "none";
+            }
+        }
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
