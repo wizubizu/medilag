7614cc8b26df42e85ae8c707e943a68384aa162e
wizubizu <odugbesanabimbola@gmail.com>
2018-09-28 18:16:28 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,21 @@ function lightbox(idx) {
                 ninjaSldr.parentNode.style.display = "none";
             }
         }
+    
+
+    // dropdown style
+        $(document).ready(function(){
+            $(".dropdown").hover(            
+                function() {
+                    $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+                    $(this).toggleClass('open');        
+                },
+                function() {
+                    $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+                    $(this).toggleClass('open');       
+                }
+            );
+        });
 
 
 
